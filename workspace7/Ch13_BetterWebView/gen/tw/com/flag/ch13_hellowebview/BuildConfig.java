79c303a78ff7b55784ac8cff826118cdb83a5385
bobo101 <bobo@fontech.com.tw>
2014-11-11 17:10:02 +0800

    v1.0

---
+++ workspace7/Ch13_BetterWebView/gen/tw/com/flag/ch13_hellowebview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tw.com.flag.ch13_hellowebview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ workspace7/Ch13_BetterWebView/src/tw/com/flag/ch13_hellowebview/MainActivity.java
@@ -0,0 +1,52 @@
+package tw.com.flag.ch13_hellowebview;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class MainActivity extends Activity  {
+	WebView wv;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_PROGRESS);  // 在標題欄加上進度功能
+		setContentView(R.layout.activity_main);
+		
+		wv = (WebView) findViewById(R.id.wv);          
+		wv.getSettings().setJavaScriptEnabled(true);	// 啟用 JavaScript	
+		wv.getSettings().setBuiltInZoomControls(true);	// 啟用縮放功能
+		wv.invokeZoomPicker();	// 縮示縮放小工具
+		wv.setWebViewClient(new WebViewClient());		// 建立及使用 WebViewClient 物件
+	    wv.setWebChromeClient(new WebChromeClient() {
+	    	public void onProgressChanged(WebView view, int progress) 	{
+	    		setProgress(progress*100);	// 顯示進度, 標題欄進度範圍是  0 到 10000
+	    	}
+	    });
+		
+		wv.loadUrl("http://www.google.com.tw");   // 連到旗標網站
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode==KeyEvent.KEYCODE_BACK &&  // 若按下的是返回鍵, 且
+				wv.canGoBack()){              // WebView 有上一頁  
+			wv.goBack();	// 回上一頁
+			return true;	// 傳回 true, 表示按鍵事件已處理
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+}
